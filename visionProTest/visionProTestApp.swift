5abab772cf9b4e4108408842cb87e78eb5a440ee
harutiro <harutiro2027@outlook.jp>
2023-08-04 14:41:53 +0900

    Initial Commit

---
+++ visionProTest/visionProTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionProTestApp.swift
+//  visionProTest
+//
+//  Created by haruto.makino on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct visionProTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
